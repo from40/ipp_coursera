c88904bc3a814b76664d36df6f5ee58e13ac99a7
from40 <didovik@gmail.com>
2020-05-01 19:14:24 +0400

    v.0.5

--- game_blackjack.py
+++ game_blackjack.py
@@ -67,7 +67,20 @@ class Hand:
 
     def get_value(self):
         # count aces as 1, if the hand has an ace, then add 10 to hand value if it doesn't bust
-        pass	# compute the value of the hand, see Blackjack video
+        hand_value = 0
+        aces = False
+        for card in self.cards_in_hand:
+            hand_value += VALUES[card.get_rank()]
+            if card.get_rank() == "A":
+                aces = True
+        if not aces:
+            return hand_value
+        else:
+            if hand_value + 10 <= 21:
+                return hand_value + 10
+            else:
+                return hand_value
+
 
     def draw(self, canvas, pos):
         pass	# draw a hand on the canvas, use the draw method for cards
@@ -156,115 +169,6 @@ frame.start()
 
 
 
-# Mini-project #6 - Blackjack
-
-import simplegui
-import random
-
-# load card sprite - 950x392 - source: jfitz.com
-CARD_SIZE = (73, 98)
-card_images = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/cards.jfitz.png")
-
-CARD_BACK_SIZE = (71, 96)
-card_back = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/card_back.png")
-
-# initialize global variables
-deck = []
-in_play = False
-outcome = ""
-score = 0
-
-# define globals for cards
-SUITS = ['C', 'S', 'H', 'D']
-RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
-VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
-
-
-# define card class
-class Card:
-    def __init__(self, suit, rank):
-        if (suit in SUITS) and (rank in RANKS):
-            self.suit = suit
-            self.rank = rank
-        else:
-            print "Invalid card: ", self.suit, self.rank
-
-    def __str__(self):
-        return self.suit + self.rank
-
-    def get_suit(self):
-        return self.suit
-
-    def get_rank(self):
-        return self.rank
-
-    def draw(self, canvas, pos):
-        card_loc = (card_size[0] * (0.5 + RANKS.index(self.rank)), card_size[1] * (0.5 + SUITS.index(self.suit)))
-        canvas.draw_image(card_images, card_loc, card_size, [pos[0] + card_size[0] / 2, pos[1] + card_size[1] / 2], card_size)
-
-# define hand class
-class Hand:
-    def __init__(self):
-        pass	# replace with your code
-
-    def __str__(self):
-        pass	# replace with your code
-
-    def add_card(self, card):
-        pass	# replace with your code
-
-    # count aces as 1, if the hand has an ace, then add 10 to hand value if don't bust
-    def get_value(self):
-        pass	# replace with your code
-        """
-        Value of Blackjack hand
-        Let hand_value be the sum of the card values with Aces as 1
-        if the hand has no Aces:
-            return hand_value
-        else:
-            if hand_value + 10 <= 21:
-                return hand_value + 10
-            else:
-                return hand_value
-
-        """
-
-    def busted(self):
-        pass	# replace with your code
-
-    def draw(self, canvas, p):
-        pass	# replace with your code
-
-
-# define deck class
-class Deck:
-    def __init__(self):
-        pass	# replace with your code
-
-    # add cards back to deck and shuffle
-    def shuffle(self):
-        pass	# replace with your code
-
-    def deal_card(self):
-        pass	# replace with your code
-
-
-#define callbacks for buttons
-def deal():
-    global outcome, in_play
-
-    # your code goes here
-
-    outcome = ""
-    in_play = True
-
-def hit():
-    pass	# replace with your code below
-
-    # if the hand is in play, hit the player
-
-    # if busted, assign an message to outcome, update in_play and score
-
 def stand():
     pass	# replace with your code below
 
@@ -274,27 +178,6 @@ def stand():
         hit the dearler's hand (use hit function)
 
     """
-
-    # assign a message to outcome, update in_play and score
-
-def draw(canvas):
-    pass	# replace with your code below
-
-
-# initialization frame
-frame = simplegui.create_frame("Blackjack", 600, 600)
-frame.set_canvas_background("Green")
-
-#create buttons and canvas callback
-frame.add_button("Deal", deal, 200)
-frame.add_button("Hit",  hit, 200)
-frame.add_button("Stand", stand, 200)
-frame.set_draw_handler(draw)
-
-# deal an initial hand
-
-# get things rolling
-frame.start()
 
 
 # Grading rubric - 18 pts total (scaled to 100)
