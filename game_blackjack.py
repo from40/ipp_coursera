6fd0861c22653a36c3cc037886dc506eb5a92e5f
from40 <didovik@gmail.com>
2020-05-01 18:41:02 +0400

    v.0.2

--- game_blackjack.py
+++ game_blackjack.py
@@ -1,5 +1,308 @@
 # http://www.codeskulptor.org/#examples-blackjack.py
+# Mini-project #6 - Blackjack
 
+import simplegui
+import random
+
+# load card sprite - 936x384 - source: jfitz.com
+CARD_SIZE = (72, 96)
+CARD_CENTER = (36, 48)
+card_images = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/cards_jfitz.png")
+
+CARD_BACK_SIZE = (72, 96)
+CARD_BACK_CENTER = (36, 48)
+card_back = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/card_jfitz_back.png")
+
+# initialize some useful global variables
+in_play = False
+outcome = ""
+score = 0
+
+# define globals for cards
+SUITS = ('C', 'S', 'H', 'D')
+RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
+VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
+
+
+# define card class
+class Card:
+    def __init__(self, suit, rank):
+        if (suit in SUITS) and (rank in RANKS):
+            self.suit = suit
+            self.rank = rank
+        else:
+            self.suit = None
+            self.rank = None
+            print "Invalid card: ", suit, rank
+
+    def __str__(self):
+        return self.suit + self.rank
+
+    def get_suit(self):
+        return self.suit
+
+    def get_rank(self):
+        return self.rank
+
+    def draw(self, canvas, pos):
+        card_loc = (CARD_CENTER[0] + CARD_SIZE[0] * RANKS.index(self.rank),
+                    CARD_CENTER[1] + CARD_SIZE[1] * SUITS.index(self.suit))
+        canvas.draw_image(card_images, card_loc, CARD_SIZE, [pos[0] + CARD_CENTER[0], pos[1] + CARD_CENTER[1]], CARD_SIZE)
+
+# define hand class
+class Hand:
+    def __init__(self):
+        self.cards_in_hand = []
+
+    def __str__(self):
+        # return a string representation of a hand
+        hands_view = ""
+        for card in self.cards_in_hand:
+            hands_view = hands_view + str(card.get_suit()) + str(card.get_rank()) + " "
+        return "Hand contains " + hands_view
+
+    def add_card(self, card):
+        # add a card object to a hand
+        self.cards_in_hand.append(card)
+
+    def get_value(self):
+        # count aces as 1, if the hand has an ace, then add 10 to hand value if it doesn't bust
+        pass	# compute the value of the hand, see Blackjack video
+
+    def draw(self, canvas, pos):
+        pass	# draw a hand on the canvas, use the draw method for cards
+
+
+# define deck class
+class Deck:
+    def __init__(self):
+        pass	# create a Deck object
+
+    def shuffle(self):
+        # shuffle the deck
+        pass    # use random.shuffle()
+
+    def deal_card(self):
+        pass	# deal a card object from the deck
+
+    def __str__(self):
+        pass	# return a string representing the deck
+
+
+
+#define event handlers for buttons
+def deal():
+    global outcome, in_play
+
+    # your code goes here
+
+    in_play = True
+
+def hit():
+    pass	# replace with your code below
+
+    # if the hand is in play, hit the player
+
+    # if busted, assign a message to outcome, update in_play and score
+
+def stand():
+    pass	# replace with your code below
+
+    # if hand is in play, repeatedly hit dealer until his hand has value 17 or more
+
+    # assign a message to outcome, update in_play and score
+
+# draw handler
+def draw(canvas):
+    # test to make sure that card.draw works, replace with your code below
+
+    card = Card("S", "A")
+    card.draw(canvas, [300, 300])
+
+
+# initialization frame
+frame = simplegui.create_frame("Blackjack", 600, 600)
+frame.set_canvas_background("Green")
+
+#create buttons and canvas callback
+frame.add_button("Deal", deal, 200)
+frame.add_button("Hit",  hit, 200)
+frame.add_button("Stand", stand, 200)
+frame.set_draw_handler(draw)
+
+
+# get things rolling
+deal()
+frame.start()
+
+
+# remember to review the gradic rubric
+
+
+
+
+
+# Mini-project #6 - Blackjack
+
+import simplegui
+import random
+
+# load card sprite - 950x392 - source: jfitz.com
+CARD_SIZE = (73, 98)
+card_images = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/cards.jfitz.png")
+
+CARD_BACK_SIZE = (71, 96)
+card_back = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/card_back.png")
+
+# initialize global variables
+deck = []
+in_play = False
+outcome = ""
+score = 0
+
+# define globals for cards
+SUITS = ['C', 'S', 'H', 'D']
+RANKS = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
+
+
+# define card class
+class Card:
+    def __init__(self, suit, rank):
+        if (suit in SUITS) and (rank in RANKS):
+            self.suit = suit
+            self.rank = rank
+        else:
+            print "Invalid card: ", self.suit, self.rank
+
+    def __str__(self):
+        return self.suit + self.rank
+
+    def get_suit(self):
+        return self.suit
+
+    def get_rank(self):
+        return self.rank
+
+    def draw(self, canvas, pos):
+        card_loc = (card_size[0] * (0.5 + RANKS.index(self.rank)), card_size[1] * (0.5 + SUITS.index(self.suit)))
+        canvas.draw_image(card_images, card_loc, card_size, [pos[0] + card_size[0] / 2, pos[1] + card_size[1] / 2], card_size)
+
+# define hand class
+class Hand:
+    def __init__(self):
+        pass	# replace with your code
+
+    def __str__(self):
+        pass	# replace with your code
+
+    def add_card(self, card):
+        pass	# replace with your code
+
+    # count aces as 1, if the hand has an ace, then add 10 to hand value if don't bust
+    def get_value(self):
+        pass	# replace with your code
+        """
+        Value of Blackjack hand
+        Let hand_value be the sum of the card values with Aces as 1
+        if the hand has no Aces:
+            return hand_value
+        else:
+            if hand_value + 10 <= 21:
+                return hand_value + 10
+            else:
+                return hand_value
+
+        """
+
+    def busted(self):
+        pass	# replace with your code
+
+    def draw(self, canvas, p):
+        pass	# replace with your code
+
+
+# define deck class
+class Deck:
+    def __init__(self):
+        pass	# replace with your code
+
+    # add cards back to deck and shuffle
+    def shuffle(self):
+        pass	# replace with your code
+
+    def deal_card(self):
+        pass	# replace with your code
+
+
+#define callbacks for buttons
+def deal():
+    global outcome, in_play
+
+    # your code goes here
+
+    outcome = ""
+    in_play = True
+
+def hit():
+    pass	# replace with your code below
+
+    # if the hand is in play, hit the player
+
+    # if busted, assign an message to outcome, update in_play and score
+
+def stand():
+    pass	# replace with your code below
+
+    # if hand is in play, repeatedly hit dealer until his hand has value 17 or more
+    """
+    while the value of the dealer's hand (use value methods) is less than 17:
+        hit the dearler's hand (use hit function)
+
+    """
+
+    # assign a message to outcome, update in_play and score
+
+def draw(canvas):
+    pass	# replace with your code below
+
+
+# initialization frame
+frame = simplegui.create_frame("Blackjack", 600, 600)
+frame.set_canvas_background("Green")
+
+#create buttons and canvas callback
+frame.add_button("Deal", deal, 200)
+frame.add_button("Hit",  hit, 200)
+frame.add_button("Stand", stand, 200)
+frame.set_draw_handler(draw)
+
+# deal an initial hand
+
+# get things rolling
+frame.start()
+
+
+# Grading rubric - 18 pts total (scaled to 100)
+
+# 1 pt - The program opens a frame with the title "Blackjack" appearing on the canvas.
+# 3 pts - The program displays 3 buttons ("Deal", "Hit" and "Stand") in the control area. (1 pt per button)
+# 2 pts - The program graphically displays the player's hand using card sprites.
+#		(1 pt if text is displayed in the console instead)
+# 2 pts - The program graphically displays the dealer's hand using card sprites.
+#		Displaying both of the dealer's cards face up is allowable when evaluating this bullet.
+#		(1 pt if text displayed in the console instead)
+# 1 pt - Hitting the "Deal" button deals out new hands to the player and dealer.
+# 1 pt - Hitting the "Hit" button deals another card to the player.
+# 1 pt - Hitting the "Stand" button deals cards to the dealer as necessary.
+# 1 pt - The program correctly recognizes the player busting.
+# 1 pt - The program correctly recognizes the dealer busting.
+# 1 pt - The program correctly computes hand values and declares a winner.
+#		Evalute based on player/dealer winner messages.
+# 1 pt - The dealer's hole card is hidden until the hand is over when it is then displayed.
+# 2 pts - The program accurately prompts the player for an action with the messages
+#        "Hit or stand?" and "New deal?". (1 pt per message)
+# 1 pt - The program keeps score correctly.
 
 
 draw_method for cards:
