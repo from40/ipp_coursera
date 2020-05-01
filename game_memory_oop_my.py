a2017118ce1b38bfa010fab4e682fe1a9b29a0ee
from40 <didovik@gmail.com>
2020-05-01 15:31:34 +0400

    v.1.0 (for futher code optimization)

--- game_memory_oop_my.py
+++ game_memory_oop_my.py
@@ -1,9 +1,8 @@
-
-# implementation of card game - Memory
-#
+import simplegui
+import random
 
 TILE_WIDTH = 50
-TILE_HEIGHT = 80
+TILE_HEIGHT = 100
 
 class Tile:
     def __init__(self, number, exp, position):
@@ -47,144 +46,74 @@ class Tile:
         return "Number is " + str(self.number) + ", exposed is " + str(self.exposed)
 
 
-
-
-
-my_tile = Tile(3)
-your_tile = Tile(4)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ___________________________ below is old code ___________________________#
-
-
-import simplegui
-import random
-
-deck = [] # list of 16 random numbers between 0 and 7 (inluded)
-state = 0 # how many cards a open at the current moment
-exposed = [] # which of the deck index card is open
-deck_x_axe = [] # tuples of range of coordinate for each card on x axe
-card1 = None # index (in the deck) of 1st open card
-card2 = None # index (in the deck) of 2nd open card
+deck = []
+state = 0
 counter_turns = 0
+first_tile = None
+second_tile = None
 
-# helper function to initialize globals
+
 def new_game():
-    global deck
+    global deck, state, counter_turns, first_tile, second_tile
+    state = 0
+    counter_turns = 0
+    first_tile = None
+    second_tile = None
+    label.set_text("Turns = " + str(counter_turns))
+
     numbers_list = [i for i in range(0, 8)]
     random.shuffle(numbers_list)
-    deck = numbers_list + numbers_list
-    random.shuffle(deck)
+    numbers_list = numbers_list + numbers_list
+    random.shuffle(numbers_list)
+    deck = []
+    for i in range(16):
+        deck.append(Tile(numbers_list[i], False, [TILE_WIDTH * i, TILE_HEIGHT]))
 
-    global state
-    state = 0
 
-    global exposed
-    exposed = [False] * 16
+def draw(canvas):
+    global deck
+    for i in range(len(deck)):
+        deck[i].draw_tile(canvas)
 
-    global deck_x_axe
-    deck_x_axe = []
-    for x1 in range(0, 800, 50):
-        deck_x_axe.append((x1, x1 + 49))
 
-    global card1, card2
-    card1 = None
-    card2 = None
-
-    global counter_turns
-    counter_turns = 0
-
-    label.set_text("Turns = 0")
-
-# define event handlers
 def mouseclick(pos):
-    global exposed, state, card1, card2, counter_turns
-    x_axis_position = pos[0]
-    # card_clicked = None
-    for i in range(len(deck_x_axe)):
-        if x_axis_position >= deck_x_axe[i][0] and \
-           x_axis_position <= deck_x_axe[i][1]:
-            #card_clicked = i
-            if exposed[i]:
+    global state, counter_turns, first_tile, second_tile
+    for tile in deck:
+        if tile.is_selected(pos):
+            if tile.is_exposed():
                 pass
             else:
-                exposed[i] = True
-                # print(card_clicked)
+                tile.expose_tile()
                 if state == 0:
                     state = 1
-                    card1 = i
+                    first_tile = tile
                 elif state == 1:
                     counter_turns += 1
                     state = 2
-                    card2 = i
+                    second_tile = tile
                 elif state == 2:
                     state = 1
-                    if deck[card1] != deck[card2]:
-                        exposed[card1] = False
-                        exposed[card2] = False
-                        card1 = i
-                        card2 = None
+                    if first_tile.get_number() != second_tile.get_number():
+                        first_tile.hide_tile()
+                        second_tile.hide_tile()
+                        first_tile = tile
+                        second_tile = None
                     else:
-                        card1 = i
-                        card2 = None
-            print("state = ", state)
-            print("card 1 = ", card1)
-            print("card 2 = ", card2, "\n")
-    label_text = "Turns = " + str(counter_turns)
-    label.set_text(label_text)
+                        first_tile = tile
+                        second_tile = None
+    label.set_text("Turns = " + str(counter_turns))
 
-
-# cards are logically 50x100 pixels in size
-def draw(canvas):
-    number_position = [0, 80]
-    for i in range(len(deck)):
-        if exposed[i]:
-            canvas.draw_text(str(deck[i]), number_position, 90, "White")
-            number_position[0] = number_position[0] + (800 / 16)
-        else:
-            canvas.draw_polygon([(number_position[0], 0),
-                                (number_position[0] + (800 / 16) - 1, 0),
-                                (number_position[0] + (800 / 16) - 1, 100),
-                                (number_position[0], 100)],
-                                1, 'Black', 'Green')
-            number_position[0] = number_position[0] + (800 / 16)
 
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
-frame.add_button("Reset", new_game)
+button = frame.add_button("Reset", new_game)
 label = frame.add_label("Turns = 0")
+
 
 # register event handlers
 frame.set_mouseclick_handler(mouseclick)
 frame.set_draw_handler(draw)
+
 
 # get things rolling
 new_game()
